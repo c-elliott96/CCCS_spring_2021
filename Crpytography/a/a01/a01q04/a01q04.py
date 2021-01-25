e453e1f6c76c942a20a05430d26b11c171289d38
Christian Elliott <cdelliott96@gmail.com>
2021-01-25 17:39:49 -0600

    finish a04

--- Crpytography/a/a01/a01q04/a01q04.py
+++ Crpytography/a/a01/a01q04/a01q04.py
@@ -1,16 +1,37 @@
 # Author: Christian Elliott
 # Date: 1-23-2021
-# Crpytography - Assignment 1
+# Crpytography - Assignment 1, Question 4
+def inv(a, n):
+    for i in range(1, n):
+        if a * i % n == 1:
+            return i
 
-# Details:
-#
-# Consider the eqaution
-#
-#        x^3 === c (mod 135246)
+    return None
 
-def q_04():
-    for i in range(1, 135246):
-        if i ** 3 % 135
-            print(i)
+MODRANGE = 135246
+def q_04(MODRANGE):
+    n = inv(27109, MODRANGE)
+    c = n * 5316 % MODRANGE
+    for x in range(MODRANGE):
+        if x ** 3 % MODRANGE == c:
+            return c
 
-q_04()
+    return None
+    
+print(q_04(MODRANGE))
+
+# ############# MICHAEL'S Solution ################
+# def new_get_solutions(space = 135246):
+#     def inv(a, n):
+#         for i in range(1, n):
+#             if a * i % n is 1:
+#                 return i
+#         return None
+#     n = inv(27109, space)
+#     c = n * 5316 % space
+#     for x in range(space):
+#         if x ** 3 % space is c: return c
+#     return None
+
+# print(new_get_solutions())
+# #################################################
